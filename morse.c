4d86f6180da8821a5afa0ec703b8488f7d24f098
KyryloMuzychka <kyryll2016@gmail.com>
2023-11-20 10:00:43 +0200

    morse.c

--- morse.c
+++ morse.c
@@ -3,7 +3,22 @@
 #include <string.h>
 #define MAX 100
 
-void Fill(char output[], int* index, const char morse[])
+
+const char *morseCodesLetters[] = {
+    ".-", "-...", "-.-.", "-..", ".",     // A-E
+    "..-.", "--.", "....", "..", ".---",  // F-J
+    "-.-", ".-..", "--", "-.", "---",     // K-O
+    ".--.", "--.-", ".-.", "...", "-",    // P-T
+    "..-", "...-", ".--", "-..-", "-.--", // U-Y
+    "--.."                                // Z
+};
+
+
+const char *morseCodesDigits[] = {
+    "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
+
+void Fill(char output[], int *index, const char morse[])
 {
     for (int i = 0; i < strlen(morse); i++)
     {
@@ -11,123 +26,152 @@ void Fill(char output[], int* index, const char morse[])
     }
 }
 
+void AddSymbol(char symbol[], const char *morseCodes[], int size, char output[], int *resultIndex, int adding)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (strcmp(symbol, morseCodes[j]) == 0)
+        {
+            output[(*resultIndex)++] = (char)(j + adding);
+            break;
+        }
+    }
+}
+
+
+int CheckSymbol(char symbol[], const char *morseCodes[], int size)
+{
+    for (int j = 0; j < size; j++)
+        if (strcmp(symbol, morseCodes[j]) == 0)
+            return 1;
+    return 0;
+}
+
 void text_to_morse(const char text[], char output[])
 {
-    const char* morseCodesLetters[] = {
-       ".-", "-...", "-.-.", "-..", ".",    // A-E
-       "..-.", "--.", "....", "..", ".---", // F-J
-       "-.-", ".-..", "--", "-.", "---",    // K-O
-       ".--.", "--.-", ".-.", "...", "-",   // P-T
-       "..-", "...-", ".--", "-..-", "-.--",// U-Y
-       "--.."                              // Z            
-    };
-
-    const char* morseCodesDigits[] = {     
-        "-----", ".----", "..---", "...--", // 0-3
-        "....-", ".....", "-....", "--...", // 4-7
-        "---..", "----."                    // 8-9    
-    };
-  
     int index = 0;
 
     for (int i = 0; i < strlen(text); i++)
-    {                     
+    {
         if ((int)toupper(text[i]) >= 65 && (int)toupper(text[i]) <= 90)
-        {            
+        {
             Fill(output, &index, morseCodesLetters[(int)toupper(text[i]) - 65]);
         }
         else if (text[i] >= 48 && text[i] <= 57)
-        {         
+        {
             Fill(output, &index, morseCodesDigits[(int)text[i] - 48]);
         }
         output[index++] = ' ';
     }
-    output[index-1] = '\0';
+
+    output[index - 1] = '\0';
 }
+
 
 void morse_to_text(const char morse[], char output[])
 {
-    const char* morseCodesLetters[] = {
-       ".-", "-...", "-.-.", "-..", ".",    // A-E
-       "..-.", "--.", "....", "..", ".---", // F-J
-       "-.-", ".-..", "--", "-.", "---",    // K-O
-       ".--.", "--.-", ".-.", "...", "-",   // P-T
-       "..-", "...-", ".--", "-..-", "-.--",// U-Y
-       "--.."                              // Z            
-    };
-
-    const char* morseCodesDigits[] = {     
-        "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."
-    };
-
     char symbol[MAX];
     int index = 0, resultIndex = 0;
+
     for (int i = 0; i < strlen(morse); i++)
-    {        
+    {
         if (morse[i] != ' ')
         {
             symbol[index++] = morse[i];
-            //printf("%c",symbol[index-1]);
         }
         else
         {
             symbol[index] = '\0';
             index = 0;
-
-            for (int j = 0; j < strlen(*morseCodesLetters); j++)
-            {
-                
-                if (strcmp(symbol, morseCodesLetters[j]) == 0)
-                {
-                    output[resultIndex++] = (char)(j+65);
-                }
-            }
-
-            for (int j = 0; j < strlen(*morseCodesDigits); j++)
-            {
-            //  printf("%s\n", morseCodesDigits[j]);
-
-                if (strcmp(symbol, morseCodesDigits[j]) == 0)
-                {
-                    output[resultIndex++] = (char)(j+65);
-                }
-            }
-            //  printf(" %s\n", symbol);
-            //printf("\n");
-        }                
+            AddSymbol(symbol, morseCodesLetters, sizeof(morseCodesLetters) / sizeof(morseCodesLetters[0]), output, &resultIndex, 65);
+            AddSymbol(symbol, morseCodesDigits, sizeof(morseCodesDigits) / sizeof(morseCodesDigits[0]), output, &resultIndex, 48);
+        }
     }
 
-    printf("%lu", strlen(*morseCodesDigits));
-
-        for (int j = 0; j < strlen(*morseCodesDigits); j++)
-        {
-            printf("%s\n", morseCodesDigits[j]);
-
-            if (strcmp(symbol, morseCodesDigits[j]) == 0)
-            {
-                output[resultIndex++] = (char)(j+65);
-            }
-        }
+    if (index > 0)
+    {
+        symbol[index] = '\0';
+        AddSymbol(symbol, morseCodesLetters, sizeof(morseCodesLetters) / sizeof(morseCodesLetters[0]), output, &resultIndex, 65);
+        AddSymbol(symbol, morseCodesDigits, sizeof(morseCodesDigits) / sizeof(morseCodesDigits[0]), output, &resultIndex, 48);
+    }
 
     output[resultIndex] = '\0';
 }
 
 
+int is_morse_code_valid(const char morse[])
+{
+
+    char symbol[MAX];
+    int index = 0, valid = 1; // resultIndex = 0;
+
+    for (int i = 0; i < strlen(morse); i++)
+    {
+        if (!valid)
+            return 0;
+
+        if (morse[i] != ' ')
+        {
+            symbol[index++] = morse[i];
+        }
+        else
+        {
+            valid = 0;
+            symbol[index] = '\0';
+            index = 0;
+
+            valid = CheckSymbol(symbol, morseCodesLetters, sizeof(morseCodesLetters) / sizeof(morseCodesLetters[0]));
+            if (!valid)
+                valid = CheckSymbol(symbol, morseCodesDigits, sizeof(morseCodesDigits) / sizeof(morseCodesDigits[0]));
+        }
+    }
+
+    if (index > 0)
+    {
+        valid = 0;
+        symbol[index] = '\0';
+        valid = CheckSymbol(symbol, morseCodesLetters, sizeof(morseCodesLetters) / sizeof(morseCodesLetters[0]));
+        if (!valid)
+            valid = CheckSymbol(symbol, morseCodesDigits, sizeof(morseCodesDigits) / sizeof(morseCodesDigits[0]));
+    }
+
+    return valid;
+}
+
 
 int main()
 {
+
     // task 5
-	//char output[150];
-	//text_to_morse("Hello", output);
-	//puts(output);
+    // char output[150];
+    // text_to_morse("Hello", output); //prints: .... . .-.. .-.. ---
+    // puts(output);
 
     // task 6
-    char output[150];
-    morse_to_text(".... . .-.. .-.. ---", output);
-    printf("morse\n");
-    printf("%s", output);
-    //prints: HELLO
-    
-	//prints: .... . .-.. .-.. ---
-	return 0;
+    // char output[150];
+    // morse_to_text(".... . .-.. .-.. --- -----", output);
+    // printf("%s\n", output); // prints: HELLO
+
+    // task 7
+    if (is_morse_code_valid(".... . .-.. .-.. ---"))
+    {
+        printf("Code is valid! \n");
+    }
+    else
+    {
+        printf("Code is invalid! \n");
+    }
+    // prints: Code is valid!
+
+    if (is_morse_code_valid(".... . .-.--. .-.. ---"))
+    {
+        printf("Code is valid! \n");
+    }
+    else
+    {
+        printf("Code is invalid! \n");
+    }
+    // prints: Code is invalid!
+
+    return 0;
 }
