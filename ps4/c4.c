6d7d180ed3bb1e5fdbb710138cdd9961915ffc1e
Kyrylo Muzychka <kyryll2016@gmail.com>
2023-11-27 21:26:50 +0200

    ps4: task9

--- ps4/c4.c
+++ ps4/c4.c
@@ -2,7 +2,6 @@
 #include "c4.h"
 #include "style.h"
 
-
 void initialize_board(int rows, int cols, char board[rows][cols])
 {
   for (int i = 0; i < rows; i++)
@@ -27,37 +26,37 @@ void print_line(int cols)
 void print_board(int rows, int cols, const char board[rows][cols])
 {
   printf(ANSI_COLOR_GREEN ANSI_BOLD);
-  
+
   print_line(cols);
-  
+
   for (int i = 0; i < rows; i++)
   {
-    printf(ANSI_COLOR_CYAN "%i ", i + 1); 
+    printf(ANSI_COLOR_CYAN "%i ", i + 1);
     printf(ANSI_COLOR_GREEN);
     for (int j = 0; j < cols; j++)
-    {      
+    {
       if (j == 0)
-        printf("|");   
+        printf("|");
 
       if (board[i][j] == '.')
-        printf(ANSI_COLOR_BLUE);      
-      else if (board[i][j] == 'X')  
-        printf(ANSI_COLOR_MAGENTA);      
-      else if (board[i][j] == '0') 
+        printf(ANSI_COLOR_BLUE);
+      else if (board[i][j] == 'X')
+        printf(ANSI_COLOR_MAGENTA);
+      else if (board[i][j] == 'O')
         printf(ANSI_COLOR_YELLOW);
-      
+
       printf(" %c ", board[i][j]);
       printf(ANSI_COLOR_GREEN "|");
     }
     printf("\n");
-    
+
     print_line(cols);
   }
-  printf(ANSI_COLOR_CYAN); 
+  printf(ANSI_COLOR_CYAN);
   printf("   ");
   for (int i = 0; i < cols; i++)
   {
-    printf(" %i  ", i + 1);   
+    printf(" %i  ", i + 1);
   }
   printf(ANSI_COLOR_RESET "\n");
 }
@@ -68,3 +67,26 @@ int is_valid_move(int rows, int cols, const char board[rows][cols], int col)
     return 0;
   return (board[0][col] == '.') ? 1 : 0;
 }
+
+int drop_piece(int rows, int cols, char board[rows][cols], int col, char player_piece)
+{
+  if (board[rows - 1][col] == '.')
+  {
+    board[rows - 1][col] = player_piece;
+    return 1;
+  }
+  else
+  {
+    for (int i = 0; i < rows; i++)
+    {
+      if (board[i][col] != '.' && i != 0)
+      {
+        board[i - 1][col] = player_piece;
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+
